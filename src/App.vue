d5c683e39b897bf3da45584f1405cf5abe21cf28
cristian <cchura.cpc@gmail.com>
2024-10-29 22:34:01 -0400

    auth login

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>MI COMPONENTE PRINCIPAL (App.vue)</h1>
+  <h2>Menú de navegación</h2>
+  <RouterLink to="/">INICIO</RouterLink> |
+  
+  <RouterLink to="/nosotros">NOSOTROS</RouterLink> |
+  
+  <RouterLink to="/servicios">SERVICIOS</RouterLink> |
+  
+  <RouterLink to="/auth/login">INGRESAR</RouterLink>
+
+  <hr>
+  <RouterView />
+
+</template>
